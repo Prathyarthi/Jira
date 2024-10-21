2061ecc460621cddce5f78f6ecee8844d762a7ca
Prathyarthi <prathyarti@gmail.com>
2024-10-21 13:46:32 +0530

    Add User button using avatar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,16 @@
-"use client"
-
 import { Button } from "@/components/ui/button";
 import { useCurrent } from "@/features/auth/api/use-current";
 import { useLogout } from "@/features/auth/api/use-logout";
+import { UserButton } from "@/features/auth/components/UserButton";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Home() {
-  const router = useRouter()
 
-  const { data, isLoading } = useCurrent()
-  const { mutate } = useLogout()
-
-  useEffect(() => {
-    if (!data && !isLoading) {
-      router.push('/signin')
-    }
-  }, [data])
 
   return (
     <div>
-      Hey There!
-
-      <Button onClick={() => mutate()}>Logout</Button>
+      <UserButton />
     </div>
   )
 }
