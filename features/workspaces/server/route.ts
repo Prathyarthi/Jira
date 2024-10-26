ece4e3ccc9c05bdb98e2283676ea990f86b2239d
Prathyarthi <prathyarti@gmail.com>
2024-10-26 19:25:46 +0530

    Add update route

--- features/workspaces/server/route.ts
+++ features/workspaces/server/route.ts
@@ -1,11 +1,12 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { createWorkspaceSchema } from "../schemas";
+import { createWorkspaceSchema, updateWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/sessionMiddleware";
 import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
 import { ID, Query } from "node-appwrite";
 import { MemberRole } from "@/features/members/types";
 import { generateInviteCode } from "@/lib/utils";
+import { getMember } from "@/features/members/utils";
 
 const app = new Hono()
     .get("/", sessionMiddleware, async (c) => {
@@ -88,6 +89,63 @@ const app = new Hono()
         return c.json({
             success: true,
             message: "Workspace created successfully",
+            data: workspace
+        })
+    })
+    .patch("/:workspaceId", sessionMiddleware, zValidator("form", updateWorkspaceSchema), async (c) => {
+
+        const databases = c.get("databases")
+        const storage = c.get("storage")
+        const user = c.get("user")
+
+        const { workspaceId } = c.req.param()
+        const { name, image } = c.req.valid("form")
+
+        const member = await getMember({
+            databases,
+            workspaceId,
+            userId: user.$id
+        })
+
+        if (!member || member.role !== MemberRole.ADMIN) {
+            return c.json({
+                error: "Unauthorized"
+            }, 401)
+        }
+
+        let uploadImageUrl: string | undefined;
+
+        if (image instanceof File) {
+            const file = await storage.createFile(
+                IMAGES_BUCKET_ID,
+                ID.unique(),
+                image
+            )
+
+            const arrayBuffer = await storage.getFilePreview(
+                IMAGES_BUCKET_ID,
+                file.$id
+            )
+
+            uploadImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`
+        }
+        else {
+            uploadImageUrl = image
+        }
+
+        const workspace = await databases.updateDocument(
+            DATABASE_ID,
+            WORKSPACES_ID,
+            workspaceId,
+            {
+                name,
+                image: uploadImageUrl,
+            }
+        )
+
+        return c.json({
+            success: true,
+            message: "Workspace updated successfully",
             data: workspace
         })
     })
